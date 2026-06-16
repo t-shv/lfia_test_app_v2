74993400b16fc748e4714b124b0741dd51b9a866
parmounks <parmounk@gmail.com>
2026-06-16 12:35:38 -0400

    gallery saver fixed

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:gallery_saver/gallery_saver.dart';
+import 'package:gal/gal.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart'
     show SystemChrome, SystemUiOverlayStyle;
@@ -615,7 +615,7 @@ class AnalysisResultFullScreen extends StatelessWidget {
     await File(imagePath).copy(newPath);
 
     try {
-      await GallerySaver.saveImage(newPath, albumName: 'LFIA Tests');
+      await Gal.putImage(newPath, album: 'LFIA Tests');
     } catch (e) {
       debugPrint('Failed to save to gallery: $e');
     }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import cloud_firestore
 import firebase_auth
 import firebase_core
+import gal
 import geolocator_apple
 import mobile_scanner
 import path_provider_foundation
@@ -18,6 +19,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
   FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
